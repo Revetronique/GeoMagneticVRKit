85bc4274751061e66a87ab920640b5188d1a98c5
Revetronique <revetronique@gmail.com>
2024-08-26 20:38:55 +0900

    [rev] enable tap input
    users can show or hide the debug panel by two-finger tap

--- Assets/GeoMagneticVRKit/Scripts/DebugPanel.cs
+++ Assets/GeoMagneticVRKit/Scripts/DebugPanel.cs
@@ -21,7 +21,8 @@ public class DebugPanel : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         //メニューボタンかDキーが押されたら
-        if (Input.GetKeyUp(KeyCode.Menu) || Input.GetKeyUp(KeyCode.D))
+        if (Input.GetKeyUp(KeyCode.Menu) || Input.GetKeyUp(KeyCode.D)
+            || (Input.GetTouch(0).phase == TouchPhase.Began && Input.touchCount > 1))
         {
             //デバッグパネルを表示
             debugMode = !debugMode;
